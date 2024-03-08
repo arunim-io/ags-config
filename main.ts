6a84467bb0d752797292ef71d5786346113044f1
Mugdha Arunim Ahmed <mugdhaarunimahmed2017@gmail.com>
2024-03-08 13:14:32 +0600

    add app launcher widget

--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
+import AppLauncher from "widgets/app-launcher/index";
 import StatusBar from "widgets/status-bar/index";
 
 export default App.config({
   icons: `${App.configDir}/assets`,
-  windows: [StatusBar()],
+  windows: [StatusBar(), AppLauncher],
 });
