f6c05ad90788950b70a850f461b1630e7a489d94
Mugdha Arunim Ahmed <mugdhaarunimahmed2017@gmail.com>
2024-04-04 09:28:49 +0600

    update audio indicator in status bar
    Signed-off-by: Mugdha Arunim Ahmed <mugdhaarunimahmed2017@gmail.com>

--- status-bar/modules/audio.ts
+++ status-bar/modules/audio.ts
@@ -2,56 +2,61 @@ import { toPercentage } from "utils";
 
 const audio = await Service.import("audio");
 
+const Mic = Widget.Box().hook(
+  audio,
+  (self) => {
+    const is_muted = audio.microphone.stream?.is_muted;
+
+    self.children = [
+      Widget.Label({ label: is_muted ? "OFF" : "ON" }),
+      Widget.Icon({
+        icon: `audio-input-microphone${is_muted ? "-muted" : ""}-symbolic`,
+      }),
+    ];
+  },
+  "microphone-changed",
+);
+
+const Sound = Widget.Button({
+  onScrollUp: () => {
+    audio.speaker.volume += 0.05;
+  },
+  onScrollDown: () => {
+    audio.speaker.volume -= 0.05;
+  },
+}).hook(
+  audio,
+  (self) => {
+    const is_muted = audio.speaker.stream?.is_muted;
+
+    self.child = Widget.Box({
+      children: [
+        Widget.Label({
+          label: is_muted
+            ? "MUTED"
+            : audio.speaker.bind("volume").as(toPercentage),
+        }),
+        Widget.Icon({
+          icon: audio.speaker.bind("volume").as((value) => {
+            const volume = value * 100;
+            const icon = [
+              [101, "overamplified"],
+              [67, "high"],
+              [34, "medium"],
+              [1, "low"],
+              [0, "muted"],
+            ].find(([threshold]) => Number(threshold) <= volume)?.[1];
+
+            return `audio-volume-${is_muted ? "muted" : icon}-symbolic`;
+          }),
+        }),
+      ],
+    });
+  },
+  "speaker-changed",
+);
+
 export default Widget.Box({
   spacing: 5,
-  children: [
-    Widget.Box({
-      children: [
-        Widget.Label().hook(
-          audio,
-          (self) => {
-            self.label = audio.microphone.stream?.is_muted ? "OFF" : "ON";
-          },
-          "microphone-changed",
-        ),
-        Widget.Icon().hook(
-          audio,
-          (self) => {
-            self.icon = `audio-input-microphone${audio.microphone.stream?.is_muted ? "-muted" : ""
-              }-symbolic`;
-          },
-          "microphone-changed",
-        ),
-      ],
-    }),
-    Widget.Button({
-      onScrollUp: () => {
-        audio.speaker.volume += 0.05;
-      },
-      onScrollDown: () => {
-        audio.speaker.volume -= 0.05;
-      },
-      child: Widget.Box({
-        children: [
-          Widget.Label({
-            label: audio.speaker.bind("volume").as(toPercentage),
-          }),
-          Widget.Icon({
-            icon: audio.speaker.bind("volume").as((value) => {
-              const volume = value * 100;
-              const icon = [
-                [101, "overamplified"],
-                [67, "high"],
-                [34, "medium"],
-                [1, "low"],
-                [0, "muted"],
-              ].find(([threshold]) => Number(threshold) <= volume)?.[1];
-
-              return `audio-volume-${icon}-symbolic`;
-            }),
-          }),
-        ],
-      }),
-    }),
-  ],
+  children: [Mic, Sound],
 });
