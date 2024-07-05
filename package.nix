816dba3f4a620e7728bf1410a557a9de3e2da827
Mugdha Arunim Ahmed <mugdhaarunimahmed2017@gmail.com>
2024-07-05 23:05:49 +0600

    rename nix package to ags

--- package.nix
+++ package.nix
@@ -1,15 +1,16 @@
 {
+  lib,
   writeShellScriptBin,
   esbuild,
   ags,
   ...
 }:
 let
-  name = "arshell";
+  name = "ags";
   src = ./.;
   dependencies = [ esbuild ];
 in
 writeShellScriptBin name ''
-  export PATH=$PATH:${builtins.concatStringsSep ":" (builtins.map (p: "${p}/bin") dependencies)}
+  export PATH=$PATH:${lib.makeBinPath dependencies}
   ${ags}/bin/ags -b ${name} -c ${src}/config.js $@
 ''
